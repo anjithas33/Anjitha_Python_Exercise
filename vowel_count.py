cd8567859786574252b5fbdb458672aa36bc5822
anjithas33 <105807009+anjithas33@users.noreply.github.com>
2022-05-20 14:58:49 +0530

    Counting the vowels

---
+++ vowel_count.py
@@ -0,0 +1,7 @@
+count1=0
+input1=input("string: ")
+for i in input1:
+    if (i=="a" or i=="e" or i=="i" or i=="o" or i=="u" 
+    or i=="A" or i=="E" or i=="I" or i=="O" or i=="U"):
+        count1=count1+1
+print("Number of vowels: ",count1)
